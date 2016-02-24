79b13b893dbffd805b5f4ec3486690ca75d754d9
Gbemisola <godblessgbemy@yahoo.com>
2016-02-24 16:39:15 +0000

    0

--- hello.php
+++ hello.php
@@ -97,33 +97,21 @@
 
 <p>
     <?php
-    $provisionedActivity = array("Specs", "Mugs", "Rock and Roll");
+    $provisionedActivity = array("Specs", "Mugs", "Sausage Roll");
     foreach($provisionedActivity as $x) {
         print "<p>$x</p>";
     }
-
-
-    ?>
-</p>
-<p>
-    <?php
-    $myarray = array("do","re","mi");
-    echo $myarray[0];
-
-    $myarray[1] = "       la";
-    echo $myarray[1];
-
-
-    unset ($myarray[2]);
-    echo $myarray[2];
-
+    $provisionedActivity[2] = "hugs";
+    unset ($provisionedActivity[3]);
+    echo $provisionedActivity[2]
 
     ?>
 </p>
 
 
 
-    ?>
+
+
 </p>
 
 
