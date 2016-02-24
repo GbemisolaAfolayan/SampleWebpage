8c673ff6c6c6d3140cee8e29b5911f715faf7193
Gbemisola <godblessgbemy@yahoo.com>
2016-02-24 15:59:58 +0000

    0

--- hello.php
+++ hello.php
@@ -53,7 +53,25 @@
     ?>
 </p>
 
+<p>
+    <?php
+    $numberofHobbits = 2;
 
+    switch ($numberofHobbits) {
+        case 1:
+            echo "1 sad hobbit";
+            break;
+        case 2:
+            echo "2 happy hobbit";
+            break;
+        case 3:
+            echo "3 hobbits are a crowd";
+            break;
+        default: "All the hobbits have gone home";
+    }
+
+    ?>
+</p>
 
 
 
