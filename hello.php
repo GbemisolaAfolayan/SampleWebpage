df89742f4d85c9a8c63c226ff39effae5ba028e3
Gbemisola <godblessgbemy@yahoo.com>
2016-02-24 15:29:41 +0000

    0

--- hello.php
+++ hello.php
@@ -28,20 +28,6 @@
         else {print "Who are you?";}
     ?>
 </p>
-<p>
-    <?php
-    $myage = "16";
-    $myage = "18";
-    $myage = "21";
-    if ($myage < "16") {print "Yor are Underage! Thanks!";}
-    if ($myage == "16") {print "You can have specs";
-    if ($myage == "18") {print "You can have specs or mugs";}
-    if ($myage >= "21") {print "You can have specs, mugs or sausage rolls";}
-    else {print "Who old are you?";}
-
-
-    ?>
-</p>
 
 
 
