23ce1ba87c82f206f88418345ef82ed87b781511
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-03-05 20:33:09 +0000

    dd

--- week4/home.php
+++ week4/home.php
@@ -10,16 +10,17 @@
 include("connection.php");
 echo"<h1> Hello". " ". $_GET["username"]."</h1>";
 
+$id=$_POST['uid'];
 $username=$_POST['username'];
 $password=$_POST['password'];
 
-$sql ="SELECT * FROM gbemisola_1515251";
+$sql ="SELECT uid, username, password FROM gbemisola_1515251";
 $result = mysqli_query($db, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
-
-        echo " Name: " . $username["username"]. " " . $password["password"]. "<br>";
+    while($sql = mysqli_fetch_assoc($result)) {
+        echo  " ID: " . $id["uid"]. "Name: " . $username["username"] . "Password: " .  $password ["password" ]. "<br>";
     }
 } else {
     echo "0 results";
