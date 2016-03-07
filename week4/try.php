929f86c15578616e54ab49ac58831fec66772648
Gbemisola <godblessgbemy@yahoo.com>
2016-03-07 15:35:07 +0000

    0

--- week4/try.php
+++ week4/try.php
@@ -2,14 +2,15 @@
 include("connection.php");
 
 
-$sql ="SELECT * FROM users";
+$sql ="SELECT * FROM gbemi";
 $result = mysqli_query($db, $sql);
 
 if (mysqli_num_rows($result) > 0) {
+    echo "i'm getting someting";
     // output data of each row
-    while($sql = mysqli_fetch_assoc($result)) {
-        echo  " ID: " . $result["uid"]. "Name: " . $result["username"] . "Password: " .  $result["password" ]. "<br>";
-    }
+    //while($sql = mysqli_fetch_assoc($result)) {
+        //echo  " ID: " . $result["uid"]. "Name: " . $result["username"] . "Password: " .  $result["password" ]. "<br>";
+    //}
 } else {
     echo "0 results";
 }
