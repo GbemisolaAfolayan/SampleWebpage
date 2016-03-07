dcc4efae19f9f3a64bfe959d14d6296e4ff4cca5
Gbemisola <godblessgbemy@yahoo.com>
2016-03-07 14:55:23 +0000

    0

--- week4/home.php
+++ week4/home.php
@@ -10,8 +10,8 @@
 include("connection.php");
 echo"<h1> Hello". " ". $_GET["username"]."</h1>";
 
-
-$sql ="SELECT * FROM users";
+$username = $_GET["username"];
+$sql ="SELECT * FROM 'users'";
 $result = mysqli_query($db, $sql);
 
 if (mysqli_num_rows($result) > 0) {
