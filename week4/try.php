3fe1f5a343b1f508350dc20415c00820e614923f
Gbemisola <godblessgbemy@yahoo.com>
2016-03-07 15:43:02 +0000

    0

--- week4/try.php
+++ week4/try.php
@@ -8,9 +8,9 @@ $result = mysqli_query($db, $sql);
 if (mysqli_num_rows($result) > 0) {
     echo "why am i not working";
     // output data of each row
-   /* while($sql = mysqli_fetch_array($result)) {
-        echo  " ID: " . $result[0]. "<br>";
-    }*/
+    while($sql = mysqli_fetch_array($result)) {
+        echo  " ID: " . $sql[0]. "<br>";
+    }
 } else {
     echo "0 results";
 }
