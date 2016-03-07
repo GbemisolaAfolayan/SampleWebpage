5d56230425c23b18906e6d4d2abdbc09959b16fb
Gbemisola <godblessgbemy@yahoo.com>
2016-03-07 15:38:21 +0000

    0

--- week4/try.php
+++ week4/try.php
@@ -9,7 +9,7 @@ if (mysqli_num_rows($result) > 0) {
 
     // output data of each row
     while($sql = mysqli_fetch_assoc($result)) {
-        echo  " ID: " . $result[0]. "<br>";
+        echo  " ID: " . $result['name']. "<br>";
     }
 } else {
     echo "0 results";
