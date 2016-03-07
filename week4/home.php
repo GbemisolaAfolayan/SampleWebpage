fdd5b637da1059c47c857ea6a9cb8143a8b947d5
Gbemisola <godblessgbemy@yahoo.com>
2016-03-07 14:32:38 +0000

    0

--- week4/home.php
+++ week4/home.php
@@ -10,8 +10,8 @@
 include("connection.php");
 echo"<h1> Hello". " ". $_GET["username"]."</h1>";
 
-
-$sql ="SELECT uid, username, password FROM gbemisola_1515251 WHERE username ='$_GET["username"]'";
+$username = $_GET["username"];
+$sql ="SELECT uid, username, password FROM gbemisola_1515251 WHERE username = $username";
 $result = mysqli_query($db, $sql);
 
 if (mysqli_num_rows($result) > 0) {
