9c9ad0470610208b67f5108fb0ccc3fe5565d3ec
Gbemisola <godblessgbemy@yahoo.com>
2016-03-07 15:37:49 +0000

    0

--- week4/try.php
+++ week4/try.php
@@ -6,11 +6,11 @@ $sql ="SELECT * FROM gbemi";
 $result = mysqli_query($db, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-    echo "i'm getting someting";
+
     // output data of each row
-    //while($sql = mysqli_fetch_assoc($result)) {
-        //echo  " ID: " . $result["uid"]. "Name: " . $result["username"] . "Password: " .  $result["password" ]. "<br>";
-    //}
+    while($sql = mysqli_fetch_assoc($result)) {
+        echo  " ID: " . $result[0]. "<br>";
+    }
 } else {
     echo "0 results";
 }
