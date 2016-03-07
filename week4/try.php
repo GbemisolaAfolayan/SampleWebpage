b7f60a05379cbf6092f1e57d11b7c8d62859f641
Gbemisola <godblessgbemy@yahoo.com>
2016-03-07 15:40:33 +0000

    0

--- week4/try.php
+++ week4/try.php
@@ -6,11 +6,11 @@ $sql ="SELECT * FROM gbemi";
 $result = mysqli_query($db, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-
+    echo "why am i not working";
     // output data of each row
-    while($sql = mysqli_fetch_array($result)) {
+   /* while($sql = mysqli_fetch_array($result)) {
         echo  " ID: " . $result[0]. "<br>";
-    }
+    }*/
 } else {
     echo "0 results";
 }
