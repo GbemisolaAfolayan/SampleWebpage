1ad55a5ef8a7e11d4e30b2886b4fd742ac019c37
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-03-12 21:24:41 +0000

    dd

---
+++ coursework1/connection.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
+define('DB_USERNAME', 'ba876eab8aa94e');
+define('DB_PASSWORD', '7fad18c8');
+define('DB_DATABASE', 'Gbemisola_1515251');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
