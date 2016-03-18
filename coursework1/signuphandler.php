54c5402a87d40d1142034b530f7b22cf91bb384a
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-03-18 14:22:09 +0000

    dd

--- coursework1/signuphandler.php
+++ coursework1/signuphandler.php
@@ -18,7 +18,7 @@ include("connection.php");
     $email=$_POST["email"];
     $phone=$_POST["phone"];
     $password=$_POST["password"];
-/*
+
     {
     //strip special characters
     $name= mysqli_real_escape_string($db,$name);
@@ -33,16 +33,16 @@ include("connection.php");
 
     $result=mysqli_query($db,$sql);
     $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-//}
+}
 if(mysqli_num_rows($result)==1) {
     echo "Sorry...This email already exists...";
 }
 
 else{
-*/
+
     $query=mysqli_query($db,"INSERT INTO users(username, email, phone, password) VALUES ('$name','$email','$phone','$password')");
 
-//}
+}
 if($query)
 {
     echo "Thank You! you are now registered.";
