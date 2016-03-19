343ed4bf76b4949aafe15110f85b8f2138bb784f
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-03-19 19:07:11 +0000

    dd

--- coursework1/bughandler.php
+++ coursework1/bughandler.php
@@ -45,6 +45,7 @@ if(mysqli_num_rows($result)==1) {
 
 else{
 */
+
     $query=mysqli_query($db,"INSERT INTO bugs(title, desc, postDate, fixDate, fixed) VALUES ('$title','$desc','$postDate','$fixDate','$fixed')");
 
     $query2=mysqli_query($db,"INSERT INTO comments (desc, postDate) VALUES ('$comment','$postDate')");
