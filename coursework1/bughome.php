8f927c4f65f8b108658ccf63f6b073d37eff019d
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-03-19 12:21:06 +0000

    dd

--- coursework1/bughome.php
+++ coursework1/bughome.php
@@ -47,12 +47,6 @@
                </tr>
            </div>
 
-            <div id="Comment">
-                <tr>
-                    <td><label for="comment">Comment: </label> </td>
-                    <td> <textarea name="comment" rows="5" cols="40"></textarea> </td>
-                </tr>
-            </div>
             <div id="submitbutton">
             <tr>
                 <td> <input type="submit" value="Submit"> </td>
---
+++ coursework1/codedump.php
@@ -0,0 +1,7 @@
+<?php
+<div id="Comment">
+                <tr>
+                    <td><label for="comment">Comment: </label> </td>
+                    <td> <textarea name="comment" rows="5" cols="40"></textarea> </td>
+                </tr>
+            </div>
