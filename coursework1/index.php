592319ddb78ac828875d785da2a2e358ede38356
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-03-23 16:32:21 +0000

    dd

--- coursework1/index.php
+++ coursework1/index.php
@@ -1,11 +1,12 @@
 
 <?php
-
 include("login.php"); //include Login script
-if ((isset($_SESSION['username']) !=''))
+session_start();
+if (isset($_SESSION['username']))
 {
     header('location: buglist.php');
 }
+
 
 ?>
 
