d7ed94cce1024b54f5bddb5b8234102f9c764af1
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-03-23 15:06:02 +0000

    dd

--- coursework1/index.php
+++ coursework1/index.php
@@ -15,7 +15,7 @@ if ((isset($_SESSION['username']) !=''))
 </head>
 <body>
 <h1> BUG TRACKING SYSTEM </h1>
-<form method="post" action="login.php">
+<form method="post" action="buglist.php">
 <div id="loginbox">
     <fieldset>
         <legend>Login</legend>
