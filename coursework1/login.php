5276f3cad9e016ff9908aeb2f16aeb0a21515b42
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-03-23 15:22:06 +0000

    dd

--- coursework1/login.php
+++ coursework1/login.php
@@ -11,53 +11,45 @@ session_start();
 include("connection.php"); //connection to database
 
 $error = ""; //Variable for storing our errors.
-/*
+
 if(isset($_POST["submit"]))
 {
-    $name = $_POST["username"];
-    $email = $_POST["email"];
-    $phone= $_POST["phone"];
-    $password = $_POST["password"];
-*/
-
-if(empty($_POST["username"]) || empty($_POST["password"]))
-{
-    $error= "Both fields are required.";
-}
-else {
-
-    $username = $_POST['username'];
-   // $email = $_POST['email'];
-   // $phone = $_POST['phone'];
-    $password = $_POST['password'];
-
-
-    // To protect from MySQL injection
-    $username = stripslashes($username);
-    $password = stripslashes($password);
-    $username = mysqli_real_escape_string($db, $username);
-    $password = mysqli_real_escape_string($db, $password);
-    $password = md5($password);
-
-    //check username and password from database
-    $sql = "SELECT userID FROM users WHERE username='$username' and password='$password'";
-    $result = mysqli_query($db, $sql);
-    $row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
-
-    //If username and password exist in our database then create a session.
-    //Otherwise echo error.
-
-    if(mysqli_num_rows($result) == 1)
+    if(empty($_POST["username"]) || empty($_POST["password"]))
     {
-        $_SESSION['username'] = $username; // Initializing Session
-        header("location: buglist.php"); // Redirecting To Other Page
-    }else
-    {
-        $error = "Incorrect username or password.";
+        $error= "Both fields are required.";
     }
+    else {
+        //define username and password
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // To protect from MySQL injection
+        $username = stripslashes($username);
+        $password = stripslashes($password);
+        $username = mysqli_real_escape_string($db, $username);
+        $password = mysqli_real_escape_string($db, $password);
+        $password = md5($password);
+
+        //check username and password from database
+        $sql = "SELECT userID FROM users WHERE username='$username' and password='$password'";
+        $result = mysqli_query($db, $sql);
+        $row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
+
+        //If username and password exist in our database then create a session.
+        //Otherwise echo error.
+
+        if(mysqli_num_rows($result) == 1)
+        {
+            $_SESSION['username'] = $username; // Initializing Session
+            header("location: buglist.php"); // Redirecting To Other Page
+        }else
+        {
+            $error = "Incorrect username or password.";
+        }
 
 
 
+    }
 }
 
 ?>
