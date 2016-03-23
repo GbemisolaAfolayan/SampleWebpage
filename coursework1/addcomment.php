58bd91cdd04dcf4aea26f0dc9764d0e57ee7b2e6
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-03-23 22:00:45 +0000

    dd

--- coursework1/addcomment.php
+++ coursework1/addcomment.php
@@ -1,5 +1,8 @@
 <?php
-
+session_start();
+include("connection.php");
+include("check.php");
+include("bug.php");
 
 ?>
 
@@ -9,27 +12,29 @@
 <body>
 <div id="content">
     <?php
-    include("connection.php");
-    include("bug.php");
+    //include("connection.php");
+   // include("bug.php");
 
-  //  if(isset($_POST["submit"]));
+   if(isset($_POST["submit"]));
 
     $comment =$_POST["comment"];
-    $postDate =$_POST["postDate"];
+    $postDate = date('d/m/y');
+    $userID=$_SESSION['userID'];
+    $bugID= $_GET["bugID"];
    // $userID =$_POST["userID"];
     //$bugID =$_POST["bugID"];
 
    // $query =  mysqli_query($db,"INSERT INTO comments (desc, postDate, userID, bugID) VALUES ('$desc', '$postDate', '$userID', '$bugID')");
-    $query =  mysqli_query($db,"INSERT INTO comments (comment, postDate) VALUES ('$comment', '$postDate')");
+    $query =  mysqli_query($db,"INSERT INTO comments (comment, postDate, userID, bugID ) VALUES ('$comment', '$postDate', '$userID', '$bugID')");
 
-    if ($query);
+    if ($query)
     {
         echo "Comments added successfully";
       //  header("location:buglist.php"); // Redirecting To another Page
     }
-    else{
-        echo "Unsuccessful...Please try again."};
-
+    else {
+        echo "Unsuccessful...Please try again.";
+    }
 
 
     //select everything from the bug table where the ID is right
