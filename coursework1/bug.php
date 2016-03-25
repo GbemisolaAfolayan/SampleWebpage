330a601ff33f4ac641362a01ff943f481b5fcf0a
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-03-25 16:11:31 +0000

    dd

--- coursework1/bug.php
+++ coursework1/bug.php
@@ -63,18 +63,18 @@ include("check.php");
     //fetch results from the database
     $result2=mysqli_query($db,$sql2);
     //we can scan through each row in the response
-   while ($row2=mysqli_fetch_assoc($result2));
-      //get the title and id from the bug
-      //$commentTitle = $row['title'];
-    $comment = $row2['comment'];
-    $postDate = $row2['postDate'];
-    $userID = $row2['userID'];
+   while ($row2=mysqli_fetch_assoc($result2)) {
+       //get the title and id from the bug
+       //$commentTitle = $row['title'];
+       $comment = $row2['comment'];
+       $postDate = $row2['postDate'];
+       $userID = $row2['userID'];
 
-    //displays the bug comment, post date and user that posted the comment
-    echo '<br>' . "Comments: " . $comment . ' ';
-    echo '<br>' . "PostDate: " . $postDate . ' ';
-    echo '<br>' . "Posted by User" . $userID . ' ';
-
+       //displays the bug comment, post date and user that posted the comment
+       echo '<br>' . "Comments: " . $comment . ' ';
+       echo '<br>' . "PostDate: " . $postDate . ' ';
+       echo '<br>' . "Posted by User" . $userID . ' ';
+   }
 
 
     ?>
