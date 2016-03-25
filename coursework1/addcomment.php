c2e614777e60cdfd2925f1a601f984ac274271c9
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-03-25 12:22:46 +0000

    dd

--- coursework1/addcomment.php
+++ coursework1/addcomment.php
@@ -26,11 +26,13 @@ include("bug.php");
     $comment= mysqli_real_escape_string($db,$comment);
 
        echo $bugID;
+    //select everything from the bug table where the ID is right
+    $sql2="SELECT *FROM bugs WHERE bugID=".$_GET["bugID"];
 
     //  $query=mysqli_query($db,"INSERT INTO bugs (title, desc, postDate, fixDate, fixed) VALUES ('$title','$desc','$postDate','$fixDate','$fixed')");
 
     //else
-  /*  $query2 = mysqli_query($db, "SELECT * FROM bugs WHERE bugID = '$bugID'") or die (mysqli_error($db));
+  //  $query2 = mysqli_query($db, "SELECT * FROM bugs WHERE bugID = '$bugID'") or die (mysqli_error($db));
 
     while ($rows = mysqli_fetch_array($query2)) {
         //$xname = $rows['username'];
@@ -40,7 +42,7 @@ include("bug.php");
         $userID= $_SESSION['userID'];
         echo "The userID of the person who posted the comment = $userID<br>";
         echo "The bugID is = $bugID<br>";
-    }*/
+    }
 
     $sql = mysqli_query ($db, "INSERT INTO comments (comment, postDate, userID, bugID) VALUES ('$comment', now(), '$userID', '$bugID')") or die(mysqli_error($db));
 
@@ -52,7 +54,7 @@ include("bug.php");
     // $getBugID=mysqli_insert_id ($db); //get bugID for the new bug
 
     // $query2=mysqli_query($db,"INSERT INTO attachments ( URL ) VALUES ('$bugfile')"
-    echo $userID;
+
     //}
 
 
