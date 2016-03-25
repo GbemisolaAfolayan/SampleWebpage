23558d02ad7eea3b3f03e5eb88bb12a9d682eba9
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-03-25 13:12:55 +0000

    dd

--- coursework1/bug.php
+++ coursework1/bug.php
@@ -37,38 +37,35 @@ include("check.php");
     $postDate=$row['postDate'];
     $fixDate=$row['fixDate'];
 
-
+    //displays the bug description, post date and fix date
     echo "<p>"."<b>".$bugTitle."</b>";
     echo "</br> Bug Description:".$bugDesc."</p>";
     echo "<p>Post Date:".$postDate."</br>";
     echo "Fix Date:".$fixDate."</p>";
 
-
-
-
+    //To display the comments on that bug, and the user that posted the comment
+    $bugID= $_GET["bugID"];
 
     //select everything from our bugs where the ID is right
-    $sql="SELECT * FROM comments WHERE bugID=".$_GET["bugID"];
+    $sql2="SELECT * FROM comments WHERE bugID=".$_GET["bugID"];
 
     //fetch results from the database
-    $result=mysqli_query($db,$sql);
-    //we can scan through rach row in the response
-    while($row=mysqli_fetch_assoc($result) & $row2=mysqli_fetch_array($db,$sql) ){
+    $result2=mysqli_query($db,$sql2);
+    //we can scan through each row in the response
+    $row2=mysqli_fetch_assoc($result2);
         //get the title and id from the bug
         //$commentTitle = $row['title'];
-        $comment = $row['comment'];
+        $comment = $row2['comment'];
         $postDate=$row2['postDate'];
         $userID=$row2['userID'];
-        //write the link to the page
-        //echo '<h3>'.$commentTitle.'</h3>';
+
+    //displays the bug comment, post date and user that posted the comment
         echo '<br>'."Comments: ".$comment.' ';
         echo '<br>'. "PostDate: ". $postDate. ' ' ;
         echo '<br>'. "Posted by User" . $userID. ' ';
-    }
 
-        //echo "a href='bug.php?id=$bugID'></a>";
-       //echo "<a href=addcomment.php?bugID=$bugID></a>";
-       echo "<a href=addcomment.php?bugID=$bugID></a>";
+
+
     ?>
 
 
