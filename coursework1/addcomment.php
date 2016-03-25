89b40fdcf9e00a0db56a191a5bb0ec1be3fcf886
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-03-25 01:33:03 +0000

    dd

--- coursework1/addcomment.php
+++ coursework1/addcomment.php
@@ -12,101 +12,25 @@ include("bug.php");
 <body>
 <div id="content">
     <?php
-    //include("connection.php");
-   // include("bug.php");
-/*
-   if(isset($_POST["submit"]));
 
-    $comment =$_POST["comment"];
-    $postDate = date('d/m/y');
-    $userID=$_SESSION['userID'];
-    $bugID= $_SESSION["bugID"];
-   // $userID =$_POST["userID"];
-    //$bugID =$_POST["bugID"];
-    echo $comment;
-    echo $postDate;
-    echo $userID;
-    echo $bugID;
-   // $query =  mysqli_query($db,"INSERT INTO comments (desc, postDate, userID, bugID) VALUES ('$desc', '$postDate', '$userID', '$bugID')");
-    $query =  mysqli_query($db,"INSERT INTO comments VALUES ('','$comment', '$postDate', '$userID', '$bugID')");
-
-    if ($query)
-    {
-        echo "Comments added successfully";
-      //  header("location:buglist.php"); // Redirecting To another Page
-    }
-    else {
-        echo "Unsuccessful...Please try again.";
-    }
-
-
-    //select everything from the bug table where the ID is right
-   // $sql="SELECT *FROM bugs WHERE bugs.ID=".$_GET["id"];
-   // $sql="INSERT INTO comments (desc, postDate, userID, bugID) VALUES ("");
-
-    //fetch result from the database
-    //$result=mysqli_query($db,$sql);
-    //scan thru each row in the response
-   // $row = mysqli_fetch_assoc($result);
-
-    //get the title and ID from the bug
-   // $bugTitle =$row['title'];
-   // $bugID =$row['bugID'];
-    //$bugDesc =$row['desc'];
-*/
-
+    //collect data from bug.php
     $comment=$_POST["comment"];
     $user=$_SESSION['username'];
-   // $descr=$_POST["descr"];
-    //$postDate=$_POST["postDate"];
-    //$fixDate='';
-    //$fixed=0;
     $userID=$_SESSION['userID'];
-    //$bugID=$_GET['bugID'];
+    $bugID=$_POST['bugID'];
     $bugID=$_GET['bugID'];
     //$bugfile=$_POST["file"];
 
-    //{
 
-    //strip special characters
-    //echo date('d/m/y');
+    //strip special characters, protect from MySQL injection
     $comment= mysqli_real_escape_string($db,$comment);
-   /* $desc= mysqli_real_escape_string($db,$desc);
-    $postDate= mysqli_real_escape_string($db,$postDate);
-    $fixDate= mysqli_real_escape_string($db,$fixDate);
-    $fixed=mysqli_real_escape_string($db,$fixed);
-    //$bugfile=mysqli_real_escape_string($db,$bugfile);
-*/
-    //$sql="SELECT email FROM users WHERE email='$email'";
 
-    //set a query to see if the entered email matches any email in the database
+       echo $bugID;
 
-    /*$result=mysqli_query($db,$sql);
-    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    //}
-    if(mysqli_num_rows($result)==1) {
-        echo "Sorry...This email already exists...";
-    }
-
-    else{
-    */
-
-    //$bugattachmentURL= addslashes($bugattachmentURL);
-    // echo $descr;
-    //  echo $title;
-    //echo $postDate;
-    //echo $fixDate;
-    //echo $fixed;
-    //echo $userID;
-    //echo $user;
-    echo $bugID;
-
-    //if (empty($_POST["file"])
-    //{
     //  $query=mysqli_query($db,"INSERT INTO bugs (title, desc, postDate, fixDate, fixed) VALUES ('$title','$desc','$postDate','$fixDate','$fixed')");
-    //}
+
     //else
-    $query2 = mysqli_query($db, "SELECT * FROM bugs WHERE bugID = '$bugID'") or die (mysqli_error($db));
+  /*  $query2 = mysqli_query($db, "SELECT * FROM bugs WHERE bugID = '$bugID'") or die (mysqli_error($db));
 
     while ($rows = mysqli_fetch_array($query2)) {
         //$xname = $rows['username'];
@@ -116,7 +40,7 @@ include("bug.php");
         $userID= $_SESSION['userID'];
         echo "The userID of the person who posted the comment = $userID<br>";
         echo "The bugID is = $bugID<br>";
-    }
+    }*/
 
     $sql = mysqli_query ($db, "INSERT INTO comments (comment, postDate, userID, bugID) VALUES ('$comment', now(), '$userID', '$bugID')") or die(mysqli_error($db));
 
