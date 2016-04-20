1943691522c37a06708d171c5ad2de3e968ca7b8
Gbemisola <godblessgbemy@yahoo.com>
2016-04-20 16:46:02 +0100

    0

--- PhotoComment/connection.php
+++ PhotoComment/connection.php
@@ -1,8 +1,16 @@
 <?php
-define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
+/*define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
 define('DB_USERNAME', 'b99256e599d9fc');
 define('DB_PASSWORD', '64c23a04');
 define('DB_DATABASE', 'rgutest');
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+//$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+*/
+
+define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net');
+define('DB_USERNAME', 'b4b5694b3860b3');
+define('DB_PASSWORD', '93badcf6');
+define('DB_DATABASE', 'Gbemisola2_1515251');
+
+$db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
--- PhotoComment/login.php
+++ PhotoComment/login.php
@@ -14,8 +14,32 @@
 			$username=$_POST['username'];
 			$password=$_POST['password'];
 
+			/*
+			$username = stripslashes($username);
+			$password = stripslashes($password);
+			$username = mysqli_real_escape_string($db, $username);
+			$password = mysqli_real_escape_string($db, $password);
+			$password = md5($password);
+			*/
 
-			
+			/* Prepare statement, stage 1: prepare */
+			if (!($stmt = $mysqli->prepare("INSERT INTO test(id) VALUES (?)"))) {
+				echo "Prepare failed: (" .  $mysqli->errno . ") " . $mysqli->error;
+			}
+
+			/*Prepared statement, stage 2: bind and execute */
+			$id = 1;
+			if (!$stmt->bind_param("i", $id)) {
+				echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+			}
+
+			/*Execute */
+
+			if (!$stmt->execute()) {
+				echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+			}
+
+
 			//Check username and password from database
 			$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
 			$result=mysqli_query($db,$sql);
