8d022e5a49a567d04355895fe238b4afb8b59bc4
Gbemisola <godblessgbemy@yahoo.com>
2016-04-20 15:09:31 +0100

    0

--- PhotoComment/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net');
-define('DB_USERNAME', 'b4b5694b3860b3');
-define('DB_PASSWORD', '93badcf6');
-define('DB_DATABASE', 'Gbemisola2_1515251');
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-
-?>
