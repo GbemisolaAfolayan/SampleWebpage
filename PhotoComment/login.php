b3257a76f2be283d142af4a9773cf3ac2e65f6fc
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-04-26 01:33:11 +0100

    0

--- PhotoComment/login.php
+++ PhotoComment/login.php
@@ -50,7 +50,8 @@
                 //If username and password exist in our database then create a session.
                 //Otherwise echo error.
 
-                if(mysqli_num_rows($result) == 1)*/ {
+                if(mysqli_num_rows($result) == 1)*/
+            {
                 $_SESSION['username'] = $username; // Initializing Session
                 header("location: photos.php"); // Redirecting To Other Page
             } else {
