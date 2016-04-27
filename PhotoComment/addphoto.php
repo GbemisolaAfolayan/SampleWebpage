70dd24515fab598a7dceb563dec78d462e373c9b
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-04-27 22:36:51 +0100

    0

--- PhotoComment/addphoto.php
+++ PhotoComment/addphoto.php
@@ -10,6 +10,16 @@ if(isset($_POST["submit"]))
     $url = "test";
     $name = $_SESSION["username"];
 
+    //escapes & strip special characters
+    $title = stripslashes($title);
+    $desc = stripslashes($desc);
+    $title = mysqli_real_escape_string($db, $title);
+    $desc = mysqli_real_escape_string($db, $desc);
+
+    // prevents xss
+    $title = htmlspecialchars($_POST["title"]);
+    $desc = htmlspecialchars($_POST["desc"]);
+
     $target_dir = "uploads/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
