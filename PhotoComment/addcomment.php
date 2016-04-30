443acc20cbb126999d41c6c84f61c1f45c7b5bbf
Oluwagbemisola <godblessgbemy@yahoo.com>
2016-04-30 13:37:47 +0100

    0

--- PhotoComment/addcomment.php
+++ PhotoComment/addcomment.php
@@ -1,34 +1,20 @@
 <?php
 session_start();
 include("connection.php"); //Establishing connection with our database
-//include("check.php");
-//xss safe output - sanitizing output
-function xecho($error){ echo xssafe($error);}
+
 $msg = ""; //Variable for storing our errors.
 if(isset($_POST["submit"]))
 {
 
-    $desc = $_POST["desc"];
-    $photoID = $_POST["photoID"];
+    $desc = ($_POST["desc"]);
+    $photoID =($_POST["photoID"]);
     $name = $_SESSION["username"];
-
-    // escape special characters
-    $desc = stripslashes($desc);
-    $photoID = stripslashes($photoID);
-    $desc = mysqli_real_escape_string($db, $desc);
-    $photoID = mysqli_real_escape_string($db, $photoID);
-
-    //prevents xss
-    $desc = htmlspecialchars($_POST["desc"]);
-    $photoID = htmlspecialchars($_POST["photoID"]);
-
 
     $sql="SELECT userID FROM users WHERE username='$name'";
     $result=mysqli_query($db,$sql);
     $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
     if(mysqli_num_rows($result) == 1) {
         //echo $name." ".$email." ".$password;
-
         $id = $row['userID'];
         $addsql = "INSERT INTO comments (description, postDate,photoID,userID) VALUES ('$desc',now(),'$photoID','$id')";
         $query = mysqli_query($db, $addsql) or die(mysqli_error($db));
